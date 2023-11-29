5dcd65516c4ead1cdb710d9ea28ff246b6a952c7
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-29 14:26:14 +0000

    deps: add deps to cliffy and cliffy modules

--- deps.ts
+++ deps.ts
@@ -7,6 +7,7 @@ import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } fro
 
 // Third Party Deno Modules
 import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
+import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
 
 // NPM Modules
 import chalk from "npm:chalk@4.1.1";
@@ -20,6 +21,7 @@ export { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg };
 
 // Third Party Deno Module Exports
 export { RepoClient, TagClient, UsersClient };
+export { Input, Select };
 
 // NPM Module Exports
 export { chalk };
