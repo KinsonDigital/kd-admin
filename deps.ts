eba179631f30864deb2d4573f73fb6b52f28b7c9
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-29 12:45:17 +0000

    deps: add unit testing dependencies

--- deps.ts
+++ deps.ts
@@ -2,9 +2,13 @@
 import chalk from "npm:chalk@4.1.1";
 import { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
 import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
-
+import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.204.0/assert/mod.ts";
+import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.204.0/testing/mock.ts";
 
 // EXPORTS
 export { existsSync }
 export { chalk };
 export { RepoClient, TagClient, UsersClient };
+
+export { assert, assertEquals, assertThrows, assertRejects, equal };
+export { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg };
