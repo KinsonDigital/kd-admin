2f5c6e1cb7a9b64f37ae54bee9c62196c1203f37
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-29 12:43:03 +0000

    deps: add additional dependencies

--- deps.ts
+++ deps.ts
@@ -1,5 +1,10 @@
+// IMPORTS
 import chalk from "npm:chalk@4.1.1";
 import { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
+import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
 
+
+// EXPORTS
 export { existsSync }
 export { chalk };
+export { RepoClient, TagClient, UsersClient };
