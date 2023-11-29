549a6df22c9c8cc166900c3b33917d673f10266f
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-29 12:42:33 +0000

    ci: add additional cicd core functionality

---
+++ .github/cicd/core/VersionPuller.ts
@@ -0,0 +1,35 @@
+import { Utils } from "../../../src/core/Utils.ts";
+import { Directory } from "../core/Directory.ts";
+
+/**
+ * Pulls the version from a json file.
+ */
+export class VersionPuller {
+	/**
+	 * Pulls the version from a json file.
+	 * @returns The version number.
+	 */
+	public getVersion(fileName: string): string {
+		const denoJsonFilePath = Directory
+			.getFiles(".", "*", true)
+			.find(f => f.endsWith(fileName));
+
+		if (denoJsonFilePath === undefined) {
+			const errorMsg = `The file '${fileName}' could not be found when pulling the version number.`;
+			Utils.printError(errorMsg);
+			Deno.exit(1);
+		}
+
+		const fileData = Deno.readTextFileSync(denoJsonFilePath);
+		const jsonObj = JSON.parse(fileData);
+
+		// If the object contains a property with the name version
+		if (jsonObj.version === undefined) {
+			const errorMsg = `The file '${fileName}' does not contain a version property.`;
+			Utils.printError(errorMsg);
+			Deno.exit(1);
+		}
+
+		return jsonObj.version;
+	}
+}
