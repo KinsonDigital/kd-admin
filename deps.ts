b607bd3687f53b306ca9ae60ad5697ce4de33b71
Calvin Wilkinson <kinsondigital@gmail.com>
2023-11-29 14:25:23 +0000

    deps,refactor: rearrange and improve dependency file

--- deps.ts
+++ deps.ts
@@ -1,14 +1,26 @@
-// IMPORTS
-import chalk from "npm:chalk@4.1.1";
+// ----IMPORTS----
+
+// Official Deno Modules
 import { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
-import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
 import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.204.0/assert/mod.ts";
 import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.204.0/testing/mock.ts";
 
-// EXPORTS
-export { existsSync }
-export { chalk };
-export { RepoClient, TagClient, UsersClient };
+// Third Party Deno Modules
+import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
 
+// NPM Modules
+import chalk from "npm:chalk@4.1.1";
+
+// ----EXPORTS----
+
+// Official Deno Module Exports
+export { existsSync }
 export { assert, assertEquals, assertThrows, assertRejects, equal };
 export { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg };
+
+// Third Party Deno Module Exports
+export { RepoClient, TagClient, UsersClient };
+
+// NPM Module Exports
+export { chalk };
+
