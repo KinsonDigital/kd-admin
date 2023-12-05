b27cb3ae692aa102c1c4c800f454fa7c05d8b0ba
Calvin Wilkinson <kinsondigital@gmail.com>
2023-12-05 18:47:33 +0000

    deps: update deno standard modules and third-party dependencies

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 // ----IMPORTS----
 
 // Official Deno Modules
-import { existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
-import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.204.0/assert/mod.ts";
-import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.204.0/testing/mock.ts";
+import { existsSync } from "https://deno.land/std@0.208.0/fs/exists.ts";
+import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.208.0/assert/mod.ts";
+import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.208.0/testing/mock.ts";
 
 // Third Party Deno Modules
 import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
