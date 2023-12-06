e03cec45f9fc50301523fdc743ec29f620e4c754
Calvin Wilkinson <kinsondigital@gmail.com>
2023-12-06 17:09:40 +0000

    chore: refactor compiler script to pack compiled file into archives

--- .github/cicd/scripts/compiler.ts
+++ .github/cicd/scripts/compiler.ts
@@ -1,3 +1,4 @@
+import { compress, existsSync } from "../../../deps.ts";
 import { CLI } from "../../../src/core/CLI.ts";
 import { ActualTarget } from "../core/types.ts";
 
@@ -40,29 +41,49 @@ if (targetMoniker != "all" && !(targetMoniker in compileTargetMonikers)) {
 const cli: CLI = new CLI();
 
 // Compile for a single os target.
-const compileTargetAsync = async (compileTarget: string): Promise<string> => {
+const compileTargetAsync = async (compileTarget: string): Promise<string | Error> => {
 	const denoCompileTarget = compileTargetMonikers[compileTarget]
 	const fileExtension = compileTarget === "win-x64" ? ".exe" : "";
 	const fileName = `kdcli${fileExtension}`;
+	const zipFileName = `kdcli-${compileTarget}.zip`;
 	const cwd = Deno.cwd();
 
-	let command = `deno compile -A --target ${denoCompileTarget} --output '${cwd}/${outputDir}/${fileName}' '${cwd}/src/main.ts'`;
+	const outputFileName = `${cwd}/${outputDir}/${fileName}`;
+
+	let command = `deno compile -A --target ${denoCompileTarget} --output '${outputFileName}' '${cwd}/src/main.ts'`;
 
 	console.log(`Compiling with the deno command:\n\t${command}`);
 	const commandResult = await cli.runAsync(command);
 
 	if (commandResult instanceof Error) {
-		return `There was an issue compiling for the target '${compileTarget}'.\n${commandResult.message}`;
+		return new Error(`There was an issue compiling for the target '${compileTarget}'.\n${commandResult.message}`);
 	}
 
-	return commandResult;
+	// Make sure that the file exists
+	if (!existsSync(outputFileName)) {
+		return new Error(`The compiled file '${outputFileName}' does not exist.`);
+	}
+
+	const zipOutputFileName = `${cwd}/${outputDir}/${zipFileName}`;
+
+	// Zip the file
+	const success = await compress(outputFileName, zipOutputFileName);
+
+	if (!success) {
+		throw new Error(`There was an issue zipping the file '${outputFileName}'.`);
+	}
+
+	// Delete the original file
+	Deno.removeSync(outputFileName);
+
+	return `${commandResult}\nSuccessfully compiled for the target '${compileTarget}'.`;
 }
 
 const compileTargets = targetMoniker === "all"
 	? Object.keys(compileTargetMonikers)
 	: [targetMoniker]
 
-const compilers: Promise<string>[] = [];
+const compilers: Promise<string | Error>[] = [];
 
 compileTargets.forEach(targetMoniker => {
 	compilers.push(compileTargetAsync(targetMoniker));
@@ -71,5 +92,9 @@ compileTargets.forEach(targetMoniker => {
 const compileResults = await Promise.all(compilers);
 
 compileResults.forEach(compileResult => {
-	console.log(compileResult);
+	const compileMsg = compileResult instanceof Error
+		? compileResult.message
+		: compileResult;
+
+	console.log(compileMsg);
 });
