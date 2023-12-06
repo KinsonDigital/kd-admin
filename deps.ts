6ef5814e381dd107647553b3aeee51c7ff7714bb
Calvin Wilkinson <kinsondigital@gmail.com>
2023-12-06 15:37:12 +0000

    deps: bring in various external dependencies

--- deps.ts
+++ deps.ts
@@ -4,10 +4,13 @@
 import { existsSync, walkSync } from "https://deno.land/std@0.208.0/fs/mod.ts";
 import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.208.0/assert/mod.ts";
 import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.208.0/testing/mock.ts";
+import { delay } from "https://deno.land/std@0.208.0/async/delay.ts";
+import * as uuid from "https://deno.land/std@0.208.0/uuid/mod.ts";
 
 // Third Party Deno Modules
 import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
-import { Input, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
+import { Confirm, Input, Secret, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
+import Progress from "https://deno.land/x/progress@v1.3.9/mod.ts";
 
 // NPM Modules
 import chalk from "npm:chalk@4.1.1";
@@ -15,13 +18,16 @@ import chalk from "npm:chalk@4.1.1";
 // ----EXPORTS----
 
 // Official Deno Module Exports
+export { delay };
 export { existsSync, walkSync }
 export { assert, assertEquals, assertThrows, assertRejects, equal };
 export { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg };
+export { uuid };
 
 // Third Party Deno Module Exports
 export { RepoClient, TagClient, UsersClient };
-export { Input, Select };
+export { Confirm, Input, Secret, Select };
+export { Progress };
 
 // NPM Module Exports
 export { chalk };
