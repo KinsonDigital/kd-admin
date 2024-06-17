5bbec379a374415b07097966b3908406862f20d6
Calvin Wilkinson <kinsondigital@gmail.com>
2024-06-17 11:20:19 +0100

    feat: create pr creator and prepare release features

--- deps.ts
+++ deps.ts
@@ -4,9 +4,14 @@
 import { existsSync, walkSync } from "https://deno.land/std@0.224.0/fs/mod.ts";
 import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.224.0/assert/mod.ts";
 import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.224.0/testing/mock.ts";
+import { toText } from "https://deno.land/std@0.218.2/streams/mod.ts";
 
 // Third Party Deno Modules
-import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.13/GitHubClients/mod.ts";
+import { RepoClient, TagClient, UsersClient, PullRequestClient, IssueClient,
+	GitClient, LabelClient, MilestoneClient, OrgClient, ProjectClient
+} from "https://deno.land/x/kd_clients@v1.0.0-preview.13/GitHubClients/mod.ts";
+import { IssueModel, PullRequestModel } from "https://deno.land/x/kd_clients@v1.0.0-preview.13/core/Models/mod.ts";
+import { IssueOrPRRequestData } from "https://deno.land/x/kd_clients@v1.0.0-preview.13/core/IssueOrPRRequestData.ts";
 import { Confirm, Input, Secret, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.4/prompt/mod.ts";
 import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
 import { crayon } from "https://deno.land/x/crayon@3.3.3/mod.ts";
@@ -17,9 +22,14 @@ import { crayon } from "https://deno.land/x/crayon@3.3.3/mod.ts";
 export { existsSync, walkSync }
 export { assert, assertEquals, assertThrows, assertRejects, equal };
 export { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg };
+export { toText };
 
 // Third Party Deno Module Exports
-export { RepoClient, TagClient, UsersClient };
+export { RepoClient, TagClient, UsersClient, PullRequestClient, IssueClient,
+	GitClient, LabelClient, MilestoneClient, OrgClient, ProjectClient
+};
+export type { IssueModel, PullRequestModel };
+export type { IssueOrPRRequestData };
 export { Confirm, Input, Secret, Select };
 export { Command };
 export { crayon };
