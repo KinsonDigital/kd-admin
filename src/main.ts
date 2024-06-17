49d11192577542518f83f52d01f97b4c4cb73dd0
Calvin Wilkinson <kinsondigital@gmail.com>
2024-06-17 11:21:12 +0100

    chore: create main entry point for the app

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
-import { chalk } from "../deps.ts";
+import { Command } from "../deps.ts";
+import { PrCreator } from "./pr-creator.ts";
 
-console.log(chalk.blue("Hello World"));
+await new Command()
+	.name("hello")
+	.description("A simple reverse proxy example cli.")
+	.version("v1.0.0")
+	.option("--create-pr", "Create a pull request.")
+	.command("create-pr")
+	.action(async () => {
+		const prCreator = new PrCreator();
+		await prCreator.createPr();
+	})
+	.command("prepare-for-release")
+	.action(() => {
+		
+	})
+	.parse();
