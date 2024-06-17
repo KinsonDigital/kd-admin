9e2c7fb1c7e6069ee739cf866444e862e40629ec
Calvin Wilkinson <kinsondigital@gmail.com>
2024-06-17 07:46:57 +0100

    deps: remove dependencies and their references

--- deps.ts
+++ deps.ts
@@ -1,17 +1,13 @@
 // ----IMPORTS----
 
 // Official Deno Modules
-import { existsSync, walkSync } from "https://deno.land/std@0.208.0/fs/mod.ts";
-import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.208.0/assert/mod.ts";
-import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.208.0/testing/mock.ts";
-import { delay } from "https://deno.land/std@0.208.0/async/delay.ts";
-import * as uuid from "https://deno.land/std@0.208.0/uuid/mod.ts";
+import { existsSync, walkSync } from "https://deno.land/std@0.224.0/fs/mod.ts";
+import { assert, assertEquals, assertThrows, assertRejects, equal } from "https://deno.land/std@0.224.0/assert/mod.ts";
+import { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg } from "https://deno.land/std@0.224.0/testing/mock.ts";
 
 // Third Party Deno Modules
-import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.5/GitHubClients/mod.ts";
-import { Confirm, Input, Secret, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
-import Progress from "https://deno.land/x/progress@v1.3.9/mod.ts";
-import { compress, decompress } from "https://deno.land/x/zip@v1.2.5/mod.ts";
+import { RepoClient, TagClient, UsersClient } from "https://deno.land/x/kd_clients@v1.0.0-preview.13/GitHubClients/mod.ts";
+import { Confirm, Input, Secret, Select } from "https://deno.land/x/cliffy@v1.0.0-rc.4/prompt/mod.ts";
 
 // NPM Modules
 import chalk from "npm:chalk@4.1.1";
@@ -19,17 +15,13 @@ import chalk from "npm:chalk@4.1.1";
 // ----EXPORTS----
 
 // Official Deno Module Exports
-export { delay };
 export { existsSync, walkSync }
 export { assert, assertEquals, assertThrows, assertRejects, equal };
 export { assertSpyCall, assertSpyCalls, spy, stub, returnsNext, returnsArg };
-export { uuid };
 
 // Third Party Deno Module Exports
 export { RepoClient, TagClient, UsersClient };
 export { Confirm, Input, Secret, Select };
-export { Progress };
-export { compress, decompress };
 
 // NPM Module Exports
 export { chalk };
