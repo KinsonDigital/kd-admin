ebc51a4c2e4725ff94ab7d6389874323a1bc2ee7
Calvin Wilkinson <kinsondigital@gmail.com>
2024-06-18 09:09:04 +0100

    chore: update playground

--- playground.ts
+++ playground.ts
@@ -1,3 +1,8 @@
-const _workingDir = Deno.args[0];
-const _token = Deno.args[1];
+const _workingDir = Deno.env.get("WORKING_DIR") ?? "";
+const _token = Deno.env.get("GITHUB_TOKEN") ?? "";
+const _ownerName = Deno.env.get("OWNER_NAME") ?? "";
+const _repoName = Deno.env.get("REPO_NAME") ?? "";
 
+
+
+debugger;
